a8dc222314e89c57773c504f212eebda95b56a13
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2023-10-25 20:09:42 -0300

    Btn-top

--- js/index.js
+++ js/index.js
@@ -8,6 +8,28 @@
 
 let burgButton = document.getElementById("burg-button");
 let burgMenu = document.getElementById("burg-menu");
+let btnTop = document.getElementById("btn-Top");
+
+btnScrollTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+
+btnTop.addEventListener("scroll", function(){
+    let element = document.getElementById("header");
+    let y = element.offsetTop;
+    let x = element.offsetLeft;
+    console.log("Element Y: " + y);
+    console.log("Element X: " + x);
+
+    if(window.scrollY(y) > 200 && btnTop.style.display === "none") {
+        btnTop.style.display = "flex"
+    } else {
+        // btnTop.style.display = "none"
+    }
+})
 
 burgButton.addEventListener("click", function() {
     if(burgMenu.style.display === "block") {
